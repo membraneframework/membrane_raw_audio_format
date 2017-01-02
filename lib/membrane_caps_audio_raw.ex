8355d545cc671917d1cb2c7644c3a6b7c75f0e44
Marcin Lewandowskig <marcin@radiokit.org>
2017-01-02 13:37:50 +0100

    Added format_to_sample_size! and sample_to_value!

--- lib/membrane_caps_audio_raw.ex
+++ lib/membrane_caps_audio_raw.ex
@@ -6,7 +6,9 @@ defmodule Membrane.Caps.Audio.Raw do
 
   @compile {:inline, [
     format_to_sample_size: 1,
-    sample_to_value: 2
+    format_to_sample_size!: 1,
+    sample_to_value: 2,
+    sample_to_value!: 2,
   ]}
 
 
@@ -55,23 +57,35 @@ defmodule Membrane.Caps.Audio.Raw do
 
   Inlined by the compiler.
   """
-  @spec format_to_sample_size(format_t) :: pos_integer
-  def format_to_sample_size(:s8), do: 1
-  def format_to_sample_size(:u8), do: 1
-  def format_to_sample_size(:s16le), do: 2
-  def format_to_sample_size(:s24le), do: 3
-  def format_to_sample_size(:s32le), do: 4
-  def format_to_sample_size(:u16le), do: 2
-  def format_to_sample_size(:u24le), do: 3
-  def format_to_sample_size(:u32le), do: 4
-  def format_to_sample_size(:s16be), do: 2
-  def format_to_sample_size(:s24be), do: 3
-  def format_to_sample_size(:s32be), do: 4
-  def format_to_sample_size(:u16be), do: 2
-  def format_to_sample_size(:u24be), do: 3
-  def format_to_sample_size(:u32be), do: 4
-  def format_to_sample_size(:f32le), do: 4
-  def format_to_sample_size(:f32be), do: 4
+  @spec format_to_sample_size(format_t) :: {:ok, pos_integer}
+  def format_to_sample_size(:s8), do: {:ok, 1}
+  def format_to_sample_size(:u8), do: {:ok, 1}
+  def format_to_sample_size(:s16le), do: {:ok, 2}
+  def format_to_sample_size(:s24le), do: {:ok, 3}
+  def format_to_sample_size(:s32le), do: {:ok, 4}
+  def format_to_sample_size(:u16le), do: {:ok, 2}
+  def format_to_sample_size(:u24le), do: {:ok, 3}
+  def format_to_sample_size(:u32le), do: {:ok, 4}
+  def format_to_sample_size(:s16be), do: {:ok, 2}
+  def format_to_sample_size(:s24be), do: {:ok, 3}
+  def format_to_sample_size(:s32be), do: {:ok, 4}
+  def format_to_sample_size(:u16be), do: {:ok, 2}
+  def format_to_sample_size(:u24be), do: {:ok, 3}
+  def format_to_sample_size(:u32be), do: {:ok, 4}
+  def format_to_sample_size(:f32le), do: {:ok, 4}
+  def format_to_sample_size(:f32be), do: {:ok, 4}
+
+
+  @doc """
+  Similar to `format_to_sample_size/1` but returns just plain value.
+
+  Inlined by the compiler.
+  """
+  @spec format_to_sample_size!(format_t) :: pos_integer
+  def format_to_sample_size!(format) do
+    {:ok, value} = format_to_sample_size(format)
+    value
+  end
 
 
   @doc """
@@ -79,84 +93,96 @@ defmodule Membrane.Caps.Audio.Raw do
 
   Inlined by the compiler.
   """
-  @spec sample_to_value(bitstring, format_t) :: integer | float
+  @spec sample_to_value(bitstring, format_t) :: {:ok, integer | float}
   def sample_to_value(sample, :s8) do
     << value :: integer-unit(8)-size(1)-signed >> = sample
-    value
+    {:ok, value}
   end
 
   def sample_to_value(sample, :u8) do
     << value :: integer-unit(8)-size(1)-unsigned >> = sample
-    value
+    {:ok, value}
   end
 
   def sample_to_value(sample, :s16le) do
     << value :: integer-unit(8)-size(2)-little-signed >> = sample
-    value
+    {:ok, value}
   end
 
   def sample_to_value(sample, :s24le) do
     << value :: integer-unit(8)-size(3)-little-signed >> = sample
-    value
+    {:ok, value}
   end
 
   def sample_to_value(sample, :s32le) do
     << value :: integer-unit(8)-size(4)-little-signed >> = sample
-    value
+    {:ok, value}
   end
 
   def sample_to_value(sample, :u16le) do
     << value :: integer-unit(8)-size(2)-little-unsigned >> = sample
-    value
+    {:ok, value}
   end
 
   def sample_to_value(sample, :u24le) do
     << value :: integer-unit(8)-size(3)-little-unsigned >> = sample
-    value
+    {:ok, value}
   end
 
   def sample_to_value(sample, :u32le) do
     << value :: integer-unit(8)-size(4)-little-unsigned >> = sample
-    value
+    {:ok, value}
   end
 
   def sample_to_value(sample, :f32le) do
     << value :: float-unit(8)-size(4)-little >> = sample
-    value
+    {:ok, value}
   end
 
   def sample_to_value(sample, :s16be) do
     << value :: integer-unit(8)-size(2)-big-signed >> = sample
-    value
+    {:ok, value}
   end
 
   def sample_to_value(sample, :s24be) do
     << value :: integer-unit(8)-size(3)-big-signed >> = sample
-    value
+    {:ok, value}
   end
 
   def sample_to_value(sample, :s32be) do
     << value :: integer-unit(8)-size(4)-big-signed >> = sample
-    value
+    {:ok, value}
   end
 
   def sample_to_value(sample, :u16be) do
     << value :: integer-unit(8)-size(2)-big-unsigned >> = sample
-    value
+    {:ok, value}
   end
 
   def sample_to_value(sample, :u24be) do
     << value :: integer-unit(8)-size(3)-big-unsigned >> = sample
-    value
+    {:ok, value}
   end
 
   def sample_to_value(sample, :u32be) do
     << value :: integer-unit(8)-size(4)-big-unsigned >> = sample
-    value
+    {:ok, value}
   end
 
   def sample_to_value(sample, :f32be) do
     << value :: float-unit(8)-size(4)-big >> = sample
+    {:ok, value}
+  end
+
+
+  @doc """
+  Similar to `sample_to_value/1` but returns just plain value.
+
+  Inlined by the compiler.
+  """
+  @spec sample_to_value!(bitstring, format_t) :: integer | float
+  def sample_to_value!(sample, format) do
+    {:ok, value} = sample_to_value(sample, format)
     value
   end
 end
--- spec/membrane_caps_audio_raw_spec.exs
+++ spec/membrane_caps_audio_raw_spec.exs
@@ -5,113 +5,113 @@ defmodule Membrane.Caps.Audio.RawSpec do
   describe ".format_to_sample_size/1" do
     context "if format is :s8" do
       let :format, do: :s8
-      it "should return 1" do
-        expect(described_module.format_to_sample_size(format)).to eq 1
+      it "should return {:ok, 1}" do
+        expect(described_module.format_to_sample_size(format)).to eq {:ok, 1}
       end
     end
 
     context "if format is :u8" do
       let :format, do: :u8
-      it "should return 1" do
-        expect(described_module.format_to_sample_size(format)).to eq 1
+      it "should return {:ok, 1}" do
+        expect(described_module.format_to_sample_size(format)).to eq {:ok, 1}
       end
     end
 
     context "if format is :s16le" do
       let :format, do: :s16le
-      it "should return 2" do
-        expect(described_module.format_to_sample_size(format)).to eq 2
+      it "should return {:ok, 2}" do
+        expect(described_module.format_to_sample_size(format)).to eq {:ok, 2}
       end
     end
 
     context "if format is :s24le" do
       let :format, do: :s24le
-      it "should return 3" do
-        expect(described_module.format_to_sample_size(format)).to eq 3
+      it "should return {:ok, 3}" do
+        expect(described_module.format_to_sample_size(format)).to eq {:ok, 3}
       end
     end
 
     context "if format is :s32le" do
       let :format, do: :s32le
-      it "should return 4" do
-        expect(described_module.format_to_sample_size(format)).to eq 4
+      it "should return {:ok, 4}" do
+        expect(described_module.format_to_sample_size(format)).to eq {:ok, 4}
       end
     end
 
     context "if format is :u16le" do
       let :format, do: :u16le
-      it "should return 2" do
-        expect(described_module.format_to_sample_size(format)).to eq 2
+      it "should return {:ok, 2}" do
+        expect(described_module.format_to_sample_size(format)).to eq {:ok, 2}
       end
     end
 
     context "if format is :u24le" do
       let :format, do: :u24le
-      it "should return 3" do
-        expect(described_module.format_to_sample_size(format)).to eq 3
+      it "should return {:ok, 3}" do
+        expect(described_module.format_to_sample_size(format)).to eq {:ok, 3}
       end
     end
 
     context "if format is :u32le" do
       let :format, do: :u32le
-      it "should return 4" do
-        expect(described_module.format_to_sample_size(format)).to eq 4
+      it "should return {:ok, 4}" do
+        expect(described_module.format_to_sample_size(format)).to eq {:ok, 4}
       end
     end
 
     context "if format is :s16be" do
       let :format, do: :s16be
-      it "should return 2" do
-        expect(described_module.format_to_sample_size(format)).to eq 2
+      it "should return {:ok, 2}" do
+        expect(described_module.format_to_sample_size(format)).to eq {:ok, 2}
       end
     end
 
     context "if format is :s24be" do
       let :format, do: :s24be
-      it "should return 3" do
-        expect(described_module.format_to_sample_size(format)).to eq 3
+      it "should return {:ok, 3}" do
+        expect(described_module.format_to_sample_size(format)).to eq {:ok, 3}
       end
     end
 
     context "if format is :s32be" do
       let :format, do: :s32be
-      it "should return 4" do
-        expect(described_module.format_to_sample_size(format)).to eq 4
+      it "should return {:ok, 4}" do
+        expect(described_module.format_to_sample_size(format)).to eq {:ok, 4}
       end
     end
 
     context "if format is :u16be" do
       let :format, do: :u16be
-      it "should return 2" do
-        expect(described_module.format_to_sample_size(format)).to eq 2
+      it "should return {:ok, 2}" do
+        expect(described_module.format_to_sample_size(format)).to eq {:ok, 2}
       end
     end
 
     context "if format is :u24be" do
       let :format, do: :u24be
-      it "should return 3" do
-        expect(described_module.format_to_sample_size(format)).to eq 3
+      it "should return {:ok, 3}" do
+        expect(described_module.format_to_sample_size(format)).to eq {:ok, 3}
       end
     end
 
     context "if format is :u32be" do
       let :format, do: :u32be
-      it "should return 4" do
-        expect(described_module.format_to_sample_size(format)).to eq 4
+      it "should return {:ok, 4}" do
+        expect(described_module.format_to_sample_size(format)).to eq {:ok, 4}
       end
     end
 
     context "if format is :f32le" do
       let :format, do: :f32le
-      it "should return 4" do
-        expect(described_module.format_to_sample_size(format)).to eq 4
+      it "should return {:ok, 4}" do
+        expect(described_module.format_to_sample_size(format)).to eq {:ok, 4}
       end
     end
 
     context "if format is :f32be" do
       let :format, do: :f32be
-      it "should return 4" do
-        expect(described_module.format_to_sample_size(format)).to eq 4
+      it "should return {:ok, 4}" do
+        expect(described_module.format_to_sample_size(format)).to eq {:ok, 4}
       end
     end
   end
@@ -122,8 +122,270 @@ defmodule Membrane.Caps.Audio.RawSpec do
       let :format, do: :s8
       let :value, do: -123
       let :sample, do: << value :: integer-unit(8)-size(1)-signed >>
+      it "should return {:ok, decoded value}" do
+        expect(described_module.sample_to_value(sample, format)).to eq {:ok, value}
+      end
+    end
+
+    context "if format is :u8" do
+      let :format, do: :u8
+      let :value, do: 123
+      let :sample, do: << value :: integer-unit(8)-size(1)-unsigned >>
+      it "should return {:ok, decoded value}" do
+        expect(described_module.sample_to_value(sample, format)).to eq {:ok, value}
+      end
+    end
+
+    context "if format is :s16le" do
+      let :format, do: :s16le
+      let :value, do: -1234
+      let :sample, do: << value :: integer-unit(8)-size(2)-little-signed >>
+      it "should return {:ok, decoded value}" do
+        expect(described_module.sample_to_value(sample, format)).to eq {:ok, value}
+      end
+    end
+
+    context "if format is :s24le" do
+      let :format, do: :s24le
+      let :value, do: -1234
+      let :sample, do: << value :: integer-unit(8)-size(3)-little-signed >>
+      it "should return {:ok, decoded value}" do
+        expect(described_module.sample_to_value(sample, format)).to eq {:ok, value}
+      end
+    end
+
+    context "if format is :s32le" do
+      let :format, do: :s32le
+      let :value, do: -1234
+      let :sample, do: << value :: integer-unit(8)-size(4)-little-signed >>
+      it "should return {:ok, decoded value}" do
+        expect(described_module.sample_to_value(sample, format)).to eq {:ok, value}
+      end
+    end
+
+    context "if format is :u16le" do
+      let :format, do: :u16le
+      let :value, do: 1234
+      let :sample, do: << value :: integer-unit(8)-size(2)-little-unsigned >>
+      it "should return {:ok, decoded value}" do
+        expect(described_module.sample_to_value(sample, format)).to eq {:ok, value}
+      end
+    end
+
+    context "if format is :u24le" do
+      let :format, do: :u24le
+      let :value, do: 1234
+      let :sample, do: << value :: integer-unit(8)-size(3)-little-unsigned >>
+      it "should return {:ok, decoded value}" do
+        expect(described_module.sample_to_value(sample, format)).to eq {:ok, value}
+      end
+    end
+
+    context "if format is :u32le" do
+      let :format, do: :u32le
+      let :value, do: 1234
+      let :sample, do: << value :: integer-unit(8)-size(4)-little-unsigned >>
+      it "should return {:ok, decoded value}" do
+        expect(described_module.sample_to_value(sample, format)).to eq {:ok, value}
+      end
+    end
+
+    context "if format is :f32le" do
+      let :format, do: :f32le
+      let :value, do: 1234.0
+      let :sample, do: << value :: float-unit(8)-size(4)-little >>
+      it "should return {:ok, decoded value}" do
+        expect(described_module.sample_to_value(sample, format)).to eq {:ok, value}
+      end
+    end
+
+    context "if format is :s16be" do
+      let :format, do: :s16be
+      let :value, do: -1234
+      let :sample, do: << value :: integer-unit(8)-size(2)-big-signed >>
+      it "should return {:ok, decoded value}" do
+        expect(described_module.sample_to_value(sample, format)).to eq {:ok, value}
+      end
+    end
+
+    context "if format is :s24be" do
+      let :format, do: :s24be
+      let :value, do: -1234
+      let :sample, do: << value :: integer-unit(8)-size(3)-big-signed >>
+      it "should return {:ok, decoded value}" do
+        expect(described_module.sample_to_value(sample, format)).to eq {:ok, value}
+      end
+    end
+
+    context "if format is :s32be" do
+      let :format, do: :s32be
+      let :value, do: -1234
+      let :sample, do: << value :: integer-unit(8)-size(4)-big-signed >>
+      it "should return {:ok, decoded value}" do
+        expect(described_module.sample_to_value(sample, format)).to eq {:ok, value}
+      end
+    end
+
+    context "if format is :u16be" do
+      let :format, do: :u16be
+      let :value, do: 1234
+      let :sample, do: << value :: integer-unit(8)-size(2)-big-unsigned >>
+      it "should return {:ok, decoded value}" do
+        expect(described_module.sample_to_value(sample, format)).to eq {:ok, value}
+      end
+    end
+
+    context "if format is :u24be" do
+      let :format, do: :u24be
+      let :value, do: 1234
+      let :sample, do: << value :: integer-unit(8)-size(3)-big-unsigned >>
+      it "should return {:ok, decoded value}" do
+        expect(described_module.sample_to_value(sample, format)).to eq {:ok, value}
+      end
+    end
+
+    context "if format is :u32be" do
+      let :format, do: :u32be
+      let :value, do: 1234
+      let :sample, do: << value :: integer-unit(8)-size(4)-big-unsigned >>
+      it "should return {:ok, decoded value}" do
+        expect(described_module.sample_to_value(sample, format)).to eq {:ok, value}
+      end
+    end
+
+    context "if format is :f32be" do
+      let :format, do: :f32be
+      let :value, do: 1234.0
+      let :sample, do: << value :: float-unit(8)-size(4)-big >>
+      it "should return {:ok, decoded value}" do
+        expect(described_module.sample_to_value(sample, format)).to eq {:ok, value}
+      end
+    end
+  end
+
+
+  describe ".format_to_sample_size!/1" do
+    context "if format is :s8" do
+      let :format, do: :s8
+      it "should return 1" do
+        expect(described_module.format_to_sample_size!(format)).to eq 1
+      end
+    end
+
+    context "if format is :u8" do
+      let :format, do: :u8
+      it "should return 1" do
+        expect(described_module.format_to_sample_size!(format)).to eq 1
+      end
+    end
+
+    context "if format is :s16le" do
+      let :format, do: :s16le
+      it "should return 2" do
+        expect(described_module.format_to_sample_size!(format)).to eq 2
+      end
+    end
+
+    context "if format is :s24le" do
+      let :format, do: :s24le
+      it "should return 3" do
+        expect(described_module.format_to_sample_size!(format)).to eq 3
+      end
+    end
+
+    context "if format is :s32le" do
+      let :format, do: :s32le
+      it "should return 4" do
+        expect(described_module.format_to_sample_size!(format)).to eq 4
+      end
+    end
+
+    context "if format is :u16le" do
+      let :format, do: :u16le
+      it "should return 2" do
+        expect(described_module.format_to_sample_size!(format)).to eq 2
+      end
+    end
+
+    context "if format is :u24le" do
+      let :format, do: :u24le
+      it "should return 3" do
+        expect(described_module.format_to_sample_size!(format)).to eq 3
+      end
+    end
+
+    context "if format is :u32le" do
+      let :format, do: :u32le
+      it "should return 4" do
+        expect(described_module.format_to_sample_size!(format)).to eq 4
+      end
+    end
+
+    context "if format is :s16be" do
+      let :format, do: :s16be
+      it "should return 2" do
+        expect(described_module.format_to_sample_size!(format)).to eq 2
+      end
+    end
+
+    context "if format is :s24be" do
+      let :format, do: :s24be
+      it "should return 3" do
+        expect(described_module.format_to_sample_size!(format)).to eq 3
+      end
+    end
+
+    context "if format is :s32be" do
+      let :format, do: :s32be
+      it "should return 4" do
+        expect(described_module.format_to_sample_size!(format)).to eq 4
+      end
+    end
+
+    context "if format is :u16be" do
+      let :format, do: :u16be
+      it "should return 2" do
+        expect(described_module.format_to_sample_size!(format)).to eq 2
+      end
+    end
+
+    context "if format is :u24be" do
+      let :format, do: :u24be
+      it "should return 3" do
+        expect(described_module.format_to_sample_size!(format)).to eq 3
+      end
+    end
+
+    context "if format is :u32be" do
+      let :format, do: :u32be
+      it "should return 4" do
+        expect(described_module.format_to_sample_size!(format)).to eq 4
+      end
+    end
+
+    context "if format is :f32le" do
+      let :format, do: :f32le
+      it "should return 4" do
+        expect(described_module.format_to_sample_size!(format)).to eq 4
+      end
+    end
+
+    context "if format is :f32be" do
+      let :format, do: :f32be
+      it "should return 4" do
+        expect(described_module.format_to_sample_size!(format)).to eq 4
+      end
+    end
+  end
+
+
+  describe ".sample_to_value!/2" do
+    context "if format is :s8" do
+      let :format, do: :s8
+      let :value, do: -123
+      let :sample, do: << value :: integer-unit(8)-size(1)-signed >>
       it "should return decoded value" do
-        expect(described_module.sample_to_value(sample, format)).to eq value
+        expect(described_module.sample_to_value!(sample, format)).to eq value
       end
     end
 
@@ -132,7 +394,7 @@ defmodule Membrane.Caps.Audio.RawSpec do
       let :value, do: 123
       let :sample, do: << value :: integer-unit(8)-size(1)-unsigned >>
       it "should return decoded value" do
-        expect(described_module.sample_to_value(sample, format)).to eq value
+        expect(described_module.sample_to_value!(sample, format)).to eq value
       end
     end
 
@@ -141,7 +403,7 @@ defmodule Membrane.Caps.Audio.RawSpec do
       let :value, do: -1234
       let :sample, do: << value :: integer-unit(8)-size(2)-little-signed >>
       it "should return decoded value" do
-        expect(described_module.sample_to_value(sample, format)).to eq value
+        expect(described_module.sample_to_value!(sample, format)).to eq value
       end
     end
 
@@ -150,7 +412,7 @@ defmodule Membrane.Caps.Audio.RawSpec do
       let :value, do: -1234
       let :sample, do: << value :: integer-unit(8)-size(3)-little-signed >>
       it "should return decoded value" do
-        expect(described_module.sample_to_value(sample, format)).to eq value
+        expect(described_module.sample_to_value!(sample, format)).to eq value
       end
     end
 
@@ -159,7 +421,7 @@ defmodule Membrane.Caps.Audio.RawSpec do
       let :value, do: -1234
       let :sample, do: << value :: integer-unit(8)-size(4)-little-signed >>
       it "should return decoded value" do
-        expect(described_module.sample_to_value(sample, format)).to eq value
+        expect(described_module.sample_to_value!(sample, format)).to eq value
       end
     end
 
@@ -168,7 +430,7 @@ defmodule Membrane.Caps.Audio.RawSpec do
       let :value, do: 1234
       let :sample, do: << value :: integer-unit(8)-size(2)-little-unsigned >>
       it "should return decoded value" do
-        expect(described_module.sample_to_value(sample, format)).to eq value
+        expect(described_module.sample_to_value!(sample, format)).to eq value
       end
     end
 
@@ -177,7 +439,7 @@ defmodule Membrane.Caps.Audio.RawSpec do
       let :value, do: 1234
       let :sample, do: << value :: integer-unit(8)-size(3)-little-unsigned >>
       it "should return decoded value" do
-        expect(described_module.sample_to_value(sample, format)).to eq value
+        expect(described_module.sample_to_value!(sample, format)).to eq value
       end
     end
 
@@ -186,7 +448,7 @@ defmodule Membrane.Caps.Audio.RawSpec do
       let :value, do: 1234
       let :sample, do: << value :: integer-unit(8)-size(4)-little-unsigned >>
       it "should return decoded value" do
-        expect(described_module.sample_to_value(sample, format)).to eq value
+        expect(described_module.sample_to_value!(sample, format)).to eq value
       end
     end
 
@@ -195,7 +457,7 @@ defmodule Membrane.Caps.Audio.RawSpec do
       let :value, do: 1234.0
       let :sample, do: << value :: float-unit(8)-size(4)-little >>
       it "should return decoded value" do
-        expect(described_module.sample_to_value(sample, format)).to eq value
+        expect(described_module.sample_to_value!(sample, format)).to eq value
       end
     end
 
@@ -204,7 +466,7 @@ defmodule Membrane.Caps.Audio.RawSpec do
       let :value, do: -1234
       let :sample, do: << value :: integer-unit(8)-size(2)-big-signed >>
       it "should return decoded value" do
-        expect(described_module.sample_to_value(sample, format)).to eq value
+        expect(described_module.sample_to_value!(sample, format)).to eq value
       end
     end
 
@@ -213,7 +475,7 @@ defmodule Membrane.Caps.Audio.RawSpec do
       let :value, do: -1234
       let :sample, do: << value :: integer-unit(8)-size(3)-big-signed >>
       it "should return decoded value" do
-        expect(described_module.sample_to_value(sample, format)).to eq value
+        expect(described_module.sample_to_value!(sample, format)).to eq value
       end
     end
 
@@ -222,7 +484,7 @@ defmodule Membrane.Caps.Audio.RawSpec do
       let :value, do: -1234
       let :sample, do: << value :: integer-unit(8)-size(4)-big-signed >>
       it "should return decoded value" do
-        expect(described_module.sample_to_value(sample, format)).to eq value
+        expect(described_module.sample_to_value!(sample, format)).to eq value
       end
     end
 
@@ -231,7 +493,7 @@ defmodule Membrane.Caps.Audio.RawSpec do
       let :value, do: 1234
       let :sample, do: << value :: integer-unit(8)-size(2)-big-unsigned >>
       it "should return decoded value" do
-        expect(described_module.sample_to_value(sample, format)).to eq value
+        expect(described_module.sample_to_value!(sample, format)).to eq value
       end
     end
 
@@ -240,7 +502,7 @@ defmodule Membrane.Caps.Audio.RawSpec do
       let :value, do: 1234
       let :sample, do: << value :: integer-unit(8)-size(3)-big-unsigned >>
       it "should return decoded value" do
-        expect(described_module.sample_to_value(sample, format)).to eq value
+        expect(described_module.sample_to_value!(sample, format)).to eq value
       end
     end
 
@@ -249,7 +511,7 @@ defmodule Membrane.Caps.Audio.RawSpec do
       let :value, do: 1234
       let :sample, do: << value :: integer-unit(8)-size(4)-big-unsigned >>
       it "should return decoded value" do
-        expect(described_module.sample_to_value(sample, format)).to eq value
+        expect(described_module.sample_to_value!(sample, format)).to eq value
       end
     end
 
@@ -258,8 +520,9 @@ defmodule Membrane.Caps.Audio.RawSpec do
       let :value, do: 1234.0
       let :sample, do: << value :: float-unit(8)-size(4)-big >>
       it "should return decoded value" do
-        expect(described_module.sample_to_value(sample, format)).to eq value
+        expect(described_module.sample_to_value!(sample, format)).to eq value
       end
     end
   end
+
 end
