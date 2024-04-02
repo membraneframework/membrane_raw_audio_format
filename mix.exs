4d55ca944373dc7d9df8d25cb3bf8cc18de9cc09
Bartek Chaliński <b.chalinski@gmail.com>
2024-04-02 19:33:15 +0200

    Add homepage link for hex docs

--- mix.exs
+++ mix.exs
@@ -6,6 +6,7 @@ defmodule Membrane.RawAudio.Mixfile do
 
   def project do
     [
+      homepage_url: "https://membrane.stream",
       app: :membrane_raw_audio_format,
       version: @version,
       elixir: "~> 1.12",
